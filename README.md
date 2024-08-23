0c86e2f23224af938b8870b097cd57c336e31fc9
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-08-23 15:47:25 -0600

    Commiting to finding a new lover

--- README.md
+++
@@ -1,4 +0,0 @@
-# Puppydevourer5000
-the newest technology in Puppy Eating 
-
-only $50,000
---
+++ scripts/variables.js
@@ -0,0 +1,8 @@
+
+// This Variable is a nimber
+let intelligenceQuotient = 100;
+
+let firstName = "Monika" ;
+let lastName = "monika" ;
+
+console.log(firstName, lastName, "has iq,", intelligenceQuotient);
