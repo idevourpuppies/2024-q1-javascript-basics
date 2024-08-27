a658fbb77f640e6762c869fc7f949393b0f6247a
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-08-27 16:02:17 -0600

    Commitment to having kids

--- scripts/variables.js
+++ scripts/variables.js
@@ -5,4 +5,26 @@ let intelligenceQuotient = 100;
 let firstName = "Monika" ;
 let lastName = "monika" ;
 
+//this vars = const
+const MIN_IQ = 50;
+const MAX_IQ = 300;
+
+//THIS VAR = OBJECT
+let myPet = {
+    type: "Domestic, short hair",
+    name: "Cali",
+    age: 11,
+    color: "calico",
+    good: "yes"
+};
+
+//this var = array
+let favCandies = [
+    "skittles",
+    "airheads",
+    "m&m's"
+];
+
+
+
 console.log(firstName, lastName, "has iq,", intelligenceQuotient);
