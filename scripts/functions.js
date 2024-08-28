7676f7463eb4a02cab22bde5e80b55abf26f4f45
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-08-28 00:33:19 -0600

    homework 1

---
+++ scripts/functions.js
@@ -0,0 +1,47 @@
+//@ts-check;
+
+// let firstName = "mica"
+
+let sayHi = function (firstName, age) {
+let firstName = f;
+let age = a;
+let greeting = 'hello my name is $(firstname) and i and $(age) years old.';
+// "Hello! my name is" + firstname + "and i am" + age + "years old.";
+console.log(greeting);
+};
+
+sayH1 ("Nicholas", 14);
+sayHi (firstName, 19);
+
+//console.log(firstName);
+
+function add(a,b) {
+    let answer = a+b;
+    return answer;
+}
+
+let additionAnswer = add("100", 5000);
+add(5000, 98733240878);
+
+console.log(additionAnswer, add(5000,98733240878));
+
+let runMyStuff = (x, y, f) =>{
+    let answer = f(x, y);
+    return answer;
+}
+
+runMyStuff(maya, 19, sayHi)
+
+let person = {
+    isWearingBarneyShirt: false,
+    isTotalyAwesome: false
+};
+
+function wearBarneyShirt(person) {
+    person.isWearingBarneyShirt = true;
+    person.isTotalyAwesome = true;
+}
+
+console.log(person);
+wearBarneyShirt(person);
+console.log(person);
