a201350379da64424f32f942d2e20ce1b35dc18a
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-09-03 16:19:37 -0600

    Commiting to sending my kids to college

--- scripts/conditionals.js
+++ scripts/conditionals.js
@@ -1,1 +1,18 @@
 //@ts-check
+
+let isTheSkyBlue = true;
+let isTheSunShing = true;
+
+if(isTheSkyBlue) {
+    isTheSunShing = false
+    //this will run if true
+    console.log("the sky is indeed blue");
+} else if(isTheSunShing) {
+    let gassColor = "green"
+    console.log("it must be night and the grass is ", gassColor)
+} else (
+    //this will run if false
+    console.log("it must be cloudy")
+)
+
+console.log(isTheSkyBlue, isTheSunShing)
