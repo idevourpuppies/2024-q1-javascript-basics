5cd1126daff159eac7ebc9857521c0c004f9b2fc
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-09-09 16:22:06 -0600

    HELP PLEASE

--- scripts/Rps.js
+++ scripts/Rps.js
@@ -2,6 +2,7 @@
 
 let playerControls = document.getElementById("player-controls");
 
+let isGameOver =false;
     let weapons = [
         {
             type: "Rock",
@@ -22,9 +23,39 @@ let playerControls = document.getElementById("player-controls");
         return weapons[Randy];
     }
 
+    function determineOutcome(playerWeapon, computerWeapon) {
+        if(playerWeapon.type === computerWeapon.type) {
+            return "It's a tie! Try again!";
+        }
+
+        isGameOver = true
+
+        if(playerWeapon.beats === computerWeapon.type) {
+            
+            return `Player wins! ${playerWeapon.type} beats ${computerWeapon.type}`;
+        }
+
+        return `Computer wins... ${computerWeapon.type} beats ${playerWeapon.type}`;
+    }
 
 function playerControlHandler(e) {
+    if (isGameOver) {
+        return
+    }
+
     let weaponName = e.target.innerText;
+
+    let playerWeapon = weapons.find(w => w.type === weaponName)
+
+    if(playerWeapon){
+        console.log("ERROR! Player weapon undefined",weaponName)
+        return;
+    }
+
+    let computerWeapon = pickRandomWeapon
+
+    let result = determineOutcome(playerWeapon, computerWeapon)
+
     console.log(weaponName, "was chosen")
 }
 
