e1e2a171b571526cea99b120c20cb3c4435c6739
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-09-12 16:23:30 -0600

    worrying what my family would say

--- scripts/Rps.js
+++ scripts/Rps.js
@@ -1,69 +1,96 @@
 //@ts-check
 
+/** @type { HTMLElement } */
+//@ts-ignore We know player controls is not null
 let playerControls = document.getElementById("player-controls");
 
-//game results
-/**@type {HTMLElement} */
-//@ts-ignore
-let gameResultsHeader = document.getElementById("game-results");
+//game-results
+/** @type { HTMLElement } */
+//@ts-ignore We know game result header is not null
+let gameResultHeader = document.getElementById("game-results");
 
-    gameResultsHeader.innerText = "Hey there";
+/** @type { HTMLElement } */
+//@ts-ignore We know game restart section is not null
+let gameRestartSection = document.getElementById("game-restart");
 
-let isGameOver =false;
-    let weapons = [
-        {
-            type: "Rock",
-            beats: "Scissors"
-        },
-        {
-            type: "Paper",
-            beats: "Rock"
-        },
-        {
-            type: "Scissors",
-            beats: "Paper"
-        }
-    ];
+gameResultHeader.innerText = "Hey there!!";
 
-    function pickRandomWeapon(weapons) {
-        let Randy = Math.floor(Math.random() * weapons.length);
-        return weapons[Randy];
-    }
+let isGameOver = false;
 
-    function determineOutcome(playerWeapon, computerWeapon) {
-        if(playerWeapon.type === computerWeapon.type) {
-            return "It's a tie! Try again!";
-        }
+let weapons = [
+	{
+		type: "Rock",
+		beats: "Scissors",
+	},
+	{
+		type: "Paper",
+		beats: "Rock",
+	},
+	{
+		type: "Scissors",
+		beats: "Paper",
+	},
+];
 
-        isGameOver = true
+function pickRandomWeapon(weapons) {
+	let randy = Math.floor(Math.random() * weapons.length);
+	return weapons[randy];
+}
 
-        if(playerWeapon.beats === computerWeapon.type) {
-            
-            return `Player wins! ${playerWeapon.type} beats ${computerWeapon.type}`;
-        }
+function determineOutcome(playerWeapon, computerWeapon) {
+	if (playerWeapon.type === computerWeapon.type) {
+		return "It's a tie! Try again!";
+	}
 
-        return `Computer wins... ${computerWeapon.type} beats ${playerWeapon.type}`;
-    }
+	isGameOver = true;
+	showRestart();
+
+	if (playerWeapon.beats === computerWeapon.type) {
+		return `Player wins! ${playerWeapon.type} beats ${computerWeapon.type}`;
+	}
+
+	return `Computer wins! ${computerWeapon.type} beats ${playerWeapon.type}`;
+}
 
 function playerControlHandler(e) {
-    // if (isGameOver) {
-    //     return
-    // }
+	if (isGameOver) {
+		return;
+	}
 
-    let weaponName = e.target.innerText;
-    let playerWeapon = weapons.find((w) => w.type === weaponName);
+	let weaponName = e.target.innerText;
+	let playerWeapon = weapons.find((w) => w.type === weaponName);
 
-    if(!playerWeapon){
-        console.log("ERROR! Player weapon undefined");
-        return;
-    }
+	if (!playerWeapon) {
+		console.log("ERROR! Player weapon undefined");
+		return;
+	}
 
-    let computerWeapon = pickRandomWeapon(weapons);
+	let computerWeapon = pickRandomWeapon(weapons);
 
-    let result = determineOutcome(playerWeapon, computerWeapon);
+	let result = determineOutcome(playerWeapon, computerWeapon);
 
-    gameResultsHeader.innerText = result;
-    console.log(result);
+	gameResultHeader.innerText = result;
+	console.log(result);
+}
+
+function gameRestartHandler(e) {
+	if (e.target.id === "btn-restart") {
+		isGameOver = false;
+		showPlayerControls();
+		gameResultHeader.innerText = "";
+	}
+}
+
+function showRestart() {
+	gameRestartSection.style.display = "initial";
+	playerControls.style.display = "none";
+}
+
+function showPlayerControls() {
+	gameRestartSection.style.display = "none";
+	playerControls.style.display = "initial";
 }
 
 playerControls?.addEventListener("click", playerControlHandler);
+
+gameRestartSection.addEventListener("click", gameRestartHandler);
