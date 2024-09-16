a0ad45b70da1176f767c0248ae57fff77e052083
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-09-16 16:22:13 -0600

    Going into hiding

---
+++ scripts/classes.js
@@ -0,0 +1,22 @@
+//@ts-check
+
+class Ball {
+    constructor(typeValue) {
+        this.type =typeValue
+        this.color = "pink"
+        this.bounce = 0;
+        this.cost = 0.0;
+    }
+
+    play() {
+        console.log(`boing ${this.type} is bouncy`)
+    }
+    
+}
+
+let red = new Ball("red ball")
+
+red.color = "red";
+
+let blue = new Ball("blue ball")
+blue.color
