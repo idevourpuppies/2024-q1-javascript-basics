362a3662123402854a29c02608a1bd417c421887
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-09-18 16:21:10 -0600

    Where did my life go wrong?

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -7,4 +7,8 @@ const canvas = document.getElementById("game-canvas");
 //@ts-ignore canvas is an HTMLCanvasElement
 const ctx = canvas.getContext("2d");
 
+    ctx.fillRect(0, 0, 50, 50);
 
+    ctx.beginPath();
+    ctx.arc(100, 100, 25, 0, Math.PI * 2);
+    ctx.fill();
