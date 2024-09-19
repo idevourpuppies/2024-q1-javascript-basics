fb89594fac3cc6a2ac16f43e09a697d67cbbf64d
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-09-19 16:26:10 -0600

    No, I know where...

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -7,8 +7,29 @@ const canvas = document.getElementById("game-canvas");
 //@ts-ignore canvas is an HTMLCanvasElement
 const ctx = canvas.getContext("2d");
 
-    ctx.fillRect(0, 0, 50, 50);
+ctx.fillStyle = "hsla(0, 100, 100%, 100%)";
+//"#ffffff"
+//"white"
 
-    ctx.beginPath();
-    ctx.arc(100, 100, 25, 0, Math.PI * 2);
-    ctx.fill();
+ctx.fillRect(0, 0, 50, 50);
+
+ctx.beginPath();
+ctx.arc(100, 100, 25, 0, Math.PI * 2);
+ctx.fill();
+
+let lastTime = 0;
+let hue = 0;
+
+function drawLoop(timestamp) {
+	let elapsedTime = timestamp - lastTime;
+	lastTime = timestamp;
+
+	ctx.fillStyle = `hsla(${hue}, 100%, 50%, 100%)`;
+	ctx.fillRect(250, 250, 50, 50);
+	hue = hue + 1;
+	console.log(elapsedTime);
+
+	window.requestAnimationFrame(drawLoop);
+}
+
+window.requestAnimationFrame(drawLoop);
