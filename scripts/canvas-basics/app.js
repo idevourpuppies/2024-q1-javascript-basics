31478200d55e86d998c066bccb6da45d0cf77bb3
idevourpuppies <Nicholasmiller82010@gmail.com>
2024-09-25 22:10:49 -0600

    So my inspiration was my username.

--- scripts/canvas-basics/app.js
+++ scripts/canvas-basics/app.js
@@ -19,12 +19,35 @@ class SquareShape {
 		this.speedMult = 10;
 		this.speedX = Math.floor(Math.random() * this.speedMult) + 1;
 		this.speedY = Math.floor(Math.random() * this.speedMult) + 1;
+
+		this.dirx = 1;
+		this.diry = 1;
 	}
 
 	update() {
-		this.x += this.speedX;
-		this.x += this.speedY;
+		this.x += this.speedX * this.dirx;
+		this.y += this.speedY * this.diry;
 		this.hue++;
+
+		if (this.hue > 360) {
+			this.hue = 0;
+		}
+
+		if (this.x < 0) {
+			// offscrean leftso move right
+			this.dirx = 1;
+		} else if (this.x + this.width > canvas.width) {
+			//offscreen right so move left
+			this.dirx = -1;
+		}
+
+		if (this.y < 0) {
+			// offscrean top so move down
+			this.diry = 1;
+		} else if (this.y + this.height > canvas.height) {
+			//offscreen bottom so move up
+			this.diry = -1;
+		}
 	}
 
 	draw() {
@@ -35,6 +58,12 @@ class SquareShape {
 
 let s1 = new SquareShape(0, 0);
 
+let shapes = [];
+
+for (let i = 0; i < 100; i++) {
+	shapes.push(new SquareShape(0, 0));
+}
+
 let lastTime = 0;
 
 function drawLoop(timestamp) {
@@ -42,10 +71,18 @@ function drawLoop(timestamp) {
 	let elapsedTime = timestamp - lastTime;
 	lastTime = timestamp;
 
-	s1.draw;
-	s1.update;
+	s1.draw();
+	s1.update();
+
+	for (const shape of shapes) {
+		shape.update();
+		shape.draw();
+	}
 
 	window.requestAnimationFrame(drawLoop);
 }
 
 window.requestAnimationFrame(drawLoop);
+function draw() {
+	throw new Error("Function not implemented.");
+}
